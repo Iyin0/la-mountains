9502d7a08d479787b3366953c39af13e7c014453
iyin0 <p2iyinoluwa@gmail.com>
2023-12-14 16:17:19 +0100

    seo optimization

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,9 +4,67 @@ import './globals.css'
 
 const oswald = Oswald({ subsets: ['latin'] })
 
+// export const metadata: Metadata = {
+//   title: 'Los Angeles Mountains',
+//   description: '',
+// }
+
 export const metadata: Metadata = {
   title: 'Los Angeles Mountains',
-  description: '',
+  description: 'Exploring the heights and wonders of nature in Los Angeles',
+  openGraph: {
+    title: 'Los Angeles Mountains',
+    description: 'Exploring the heights and wonders of nature in Los Angeles',
+    url: 'https://la-mountains-flax.vercel.app/',
+    siteName: 'Los Angeles Mountains',
+    images: [
+      {
+        url: './assets/FooterLogo.png',
+        width: 800,
+        height: 600,
+      },
+      {
+        url: './assets/Logo-text.png',
+        width: 1800,
+        height: 1600,
+        alt: 'Los Angeles Mountains',
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
+  twitter: {
+    card: 'app',
+    title: 'Los Angeles Mountains',
+    description: 'Exploring the heights and wonders of nature in Los Angeles',
+    site: '@iyin___',
+    creator: '@iyin___',
+    images: {
+      url: './assets/FooterLogo.png',
+      alt: 'Los Angeles Mountains',
+    },
+    app: {
+      name: 'Los Angeles Mountains',
+      id: {
+        iphone: '',
+        ipad: '',
+        googleplay: '',
+      },
+      url: {
+        iphone: '',
+        ipad: '',
+      },
+    },
+  },
+  alternates: {
+    canonical: 'https://la-mountains-flax.vercel.app/',
+  },
+  appLinks: {
+    web: {
+      url: 'https://la-mountains-flax.vercel.app/',
+      should_fallback: true,
+    },
+  }
 }
 
 export default function RootLayout({
