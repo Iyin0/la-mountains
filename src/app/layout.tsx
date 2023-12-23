e888a39fc48e08a8742b312edeac01232d5d50e0
iyin0 <p2iyinoluwa@gmail.com>
2023-12-23 08:30:48 +0100

    some changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Oswald } from 'next/font/google'
 import './globals.css'
+import { Analytics } from '@vercel/analytics/react';
 
 const oswald = Oswald({ subsets: ['latin'] })
 
@@ -74,7 +75,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={oswald.className}>{children}</body>
+      <body className={oswald.className}>
+        {children}
+        <Analytics />
+      </body>
     </html>
   )
 }
