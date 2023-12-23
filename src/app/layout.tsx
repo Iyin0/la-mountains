cf8cfecaa912aae436539a5e3a41f33f9c0e9c77
iyin0 <p2iyinoluwa@gmail.com>
2023-12-23 08:35:19 +0100

    some changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -76,8 +76,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={oswald.className}>
-        {children}
         <Analytics />
+        {children}
       </body>
     </html>
   )
